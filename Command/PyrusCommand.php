c37b802ba3554d51c83f32b67f5d8b391a0009ed
Nils Adermann <naderman@naderman.de>
2011-01-09 07:23:03 +0100

    Move Pyrus access mechanisms to a separate class that is not tied to a command.

--- Command/PyrusCommand.php
+++ Command/PyrusCommand.php
@@ -15,6 +15,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Bundle\Phpbb\PyrusBundle\Input\ArbitraryInputDefinition;
+use Bundle\Phpbb\PyrusBundle\Pyrus;
 
 /**
  * Pyrus command
@@ -32,35 +33,12 @@ class PyrusCommand extends Command
             ->setDefinition($this->inputDefinition);
     }
 
-    protected function getPyrusDir()
-    {
-        $bundleDirs = $this->application->getKernel()->getBundleDirs();
-
-        if (!isset($bundleDirs['Bundle'])) {
-            throw new \RunTimeException("No BundleDir for namespace Bundle defined in Application Kernel.");
-        }
-
-        return $bundleDirs['Bundle'];
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $tokens = $this->inputDefinition->getTokens($input);
         array_shift($tokens); // remove pyrus
 
-        array_unshift($tokens, $this->getPyrusDir());
-
-        spl_autoload_register(array($this, 'pyrus_autoload'));
-        $frontend = new \PEAR2\Pyrus\ScriptFrontend\Commands;
-        $frontend->run($tokens);
+        $pyrus = new Pyrus($this->application->getKernel());
+        $pyrus->run($tokens);
     }
-
-    function pyrus_autoload($class)
-    {
-        $class = str_replace(array('_','\\'), '/', $class);
-        if (file_exists('phar://' . __DIR__ . '/../pyrus.phar/PEAR2_Pyrus-2.0.0a2/php/' . $class . '.php')) {
-            include 'phar://' . __DIR__ . '/../pyrus.phar/PEAR2_Pyrus-2.0.0a2/php/' . $class . '.php';
-        }
-    }
-
 }
---
+++ Pyrus.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * This file is part of phpBB.
+ *
+ * (c) phpBB Ltd.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Bundle\Phpbb\PyrusBundle;
+use Symfony\Component\HttpKernel\Kernel;
+
+/**
+ * Provides simplified access to Pyrus in symfony environment
+ */
+class Pyrus
+{
+    protected $kernel;
+
+    /**
+     * Sets Pyrus up for later use
+     *
+     * @param Kernel $kernel The application's kernel
+     */
+    public function __construct(Kernel $kernel)
+    {
+        $this->kernel = $kernel;
+        spl_autoload_register(array($this, 'pyrus_autoload'));
+    }
+
+    /**
+     * Returns the directory used for Pyrus packages
+     *
+     * @return string The bundle directory
+     */
+    public function getPyrusDir()
+    {
+        $bundleDirs = $this->kernel->getBundleDirs();
+
+        if (!isset($bundleDirs['Bundle'])) {
+            throw new \RunTimeException("No BundleDir for namespace Bundle defined in Application Kernel.");
+        }
+
+        return $bundleDirs['Bundle'];
+    }
+
+    /**
+     * Executes a Pyrus command through its CLI frontend in the bundle directory.
+     *
+     * @param array $argv The list of options and arguments, not including the
+     *                    executing script.
+     */
+    public function run(array $argv)
+    {
+        array_unshift($argv, $this->getPyrusDir());
+
+        $frontend = new \PEAR2\Pyrus\ScriptFrontend\Commands;
+        $frontend->run($argv);
+    }
+
+    /**
+    * The Pyrus autoloader taking classes from the phar file within this Bundle.
+    *
+    * @param string $class The class name
+    */
+    public function pyrus_autoload($class)
+    {
+        $class = str_replace(array('_','\\'), '/', $class);
+        if (file_exists('phar://' . __DIR__ . '/pyrus.phar/PEAR2_Pyrus-2.0.0a2/php/' . $class . '.php')) {
+            include 'phar://' . __DIR__ . '/pyrus.phar/PEAR2_Pyrus-2.0.0a2/php/' . $class . '.php';
+        }
+    }
+}
