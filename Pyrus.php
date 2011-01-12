393de3fc3d63d751da5e3eeb4ef41ce216f36d04
Nils Adermann <naderman@naderman.de>
2011-01-12 02:50:26 +0100

    Make the Pyrus environment entirely self contained within the symfony install.
    Added a custom role for bundle files which is automatically installed on setup.

--- Pyrus.php
+++ Pyrus.php
@@ -35,14 +35,18 @@ class Pyrus
         $this->kernel = $kernel;
         $this->frontend = $frontend;
 
-        $configclass = $frontend::$configclass;
-        $this->config = $configclass::singleton($this->getPyrusDir());
+        $this->config = \PEAR2\Pyrus\Config::singleton($this->getPyrusDir(), $this->getPearConfig());
 
         $this->setupPyrus();
     }
 
     public function setupPyrus()
     {
+        if (!file_exists($this->getPearConfig()))
+        {
+            $config->saveConfig($this->getPearConfig());
+        }
+/*
         $settings = array(
             //'php_dir' => '@php_dir@',
             'ext_dir' => '@php_dir@/pyrus/ext',
@@ -68,7 +72,9 @@ class Pyrus
             if ($currentValue != $desiredValue) {
                 $this->setConfig($name, $desiredValue);
             }
-        }
+        }*/
+
+        $this->installCustomRole();
     }
 
     /**
@@ -92,15 +98,66 @@ class Pyrus
     public function setConfig($name, $value)
     {
         $this->config->__set($name, $value);
-        $this->config->saveConfig();
+        $this->config->saveConfig($this->getPearConfig());
+    }
+
+    public function installCustomRole()
+    {
+        $pf = new \PEAR2\Pyrus\PackageFile\v2;
+
+        $pf->name = 'PyrusBundlePlugin';
+        $pf->channel = 'pear2.php.net';
+        $pf->summary = 'Local';
+        $pf->description = 'Description';
+        $pf->notes = 'My Notes';
+        $pf->maintainer['naderman']->role('lead')->email('naderman@phpbb.com')->active('yes')->name('Nils Adermann');
+        $pf->files['bundle.xml'] = array(
+            'attribs' => array('role' => 'customrole'),
+        );
+        $pf->files['PyrusBundlePlugin/BundleRole.php'] = array(
+            'attribs' => array('role' => 'php'),
+        );
+
+        $package_xml = __DIR__ . '/plugin/package.xml';
+        $pf->setPackagefile($package_xml);
+
+        $package = new \PEAR2\Pyrus\Package(false);
+        $xmlcontainer = new \PEAR2\Pyrus\PackageFile($pf);
+        $xml = new \PEAR2\Pyrus\Package\Xml($package_xml, $package, $xmlcontainer);
+        $package->setInternalPackage($xml);
+        \PEAR2\Pyrus\Main::$options['install-plugins'] = true;
+
+        \PEAR2\Pyrus\Installer::begin();
+        \PEAR2\Pyrus\Installer::prepare($package);
+        \PEAR2\Pyrus\Installer::commit();
     }
 
     /**
      * Returns the directory used for Pyrus packages
      *
-     * @return string The bundle directory
+     * @return string The vendor directory
      */
     public function getPyrusDir()
+    {
+        return $this->kernel->getRootDir() . '/../src/vendor';
+    }
+
+    /**
+     * Retrieve path to pearconfig.xml user configuration
+     *
+     * @return string Path to pearconfig.xml
+     */
+    public function getPearConfig()
+    {
+        return $this->getPyrusDir() . '/pearconfig.xml';
+    }
+
+    /**
+     * Returns the directory to be used for Pyrus installed Bundles.
+     *
+     * @return string The bundle directory
+     */
+    public function getBundleDir()
     {
         $bundleDirs = $this->kernel->getBundleDirs();
 
---
+++ plugin/PyrusBundlePlugin/BundleRole.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of phpBB.
+ *
+ * (c) phpBB Ltd.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * bundle role
+ */
+namespace PEAR2\Pyrus\Installer\Role;
+class Php extends \PEAR2\Pyrus\Installer\Role\Common {}
